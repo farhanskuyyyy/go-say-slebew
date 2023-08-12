846723bd4b89ee6bd39b7b8d2d19beb06737a5ee
farhanlumoshive <farhan.arfianto@lumoshive.com>
2023-08-12 11:12:29 +0700

    tambah satu huruf

--- say_slebew.go
+++ say_slebew.go
@@ -1,5 +1,13 @@
 package go_say_slebew
 
+
+// cara bikin module trus di upload ke github 
+// go mod init dl , trus bikin source codenya 
+// commit , trus push ke github 
+// push tag jg caranya 
+// git tag v1.0.0
+// git push origin v1.0.0
+
 func SaySlebew() string {
-	return "Slebewwww"
+	return "Slebewwwww"
 }
